55832c43dd1ea6973c5fd835c7d0a310443392d7
Josh <josh@joshrweaver.com>
2025-01-07 11:34:22 -0600

    Enhance: Vitest configuration and update Codecov workflow
    - Added default JWT_SECRET and POSTGRESQL_URL environment variables in vitest.config.ts.
    - Configured Vitest to use JUnit reporters and specified output file for test reports.
    - Updated Codecov workflow to include POSTGRESQL_URL in the environment variables.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import path from 'path'
 
+process.env.JWT_SECRET = process.env.JWT_SECRET || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+process.env.POSTGRESQL_URL = process.env.POSTGRESQL_URL || 'postgresql://user:password@localhost:5432/mydatabase';
+
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -12,6 +16,10 @@ export default defineConfig({
     environment: 'node',
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: './test-report.junit.xml'
+    },
     coverage: {
       provider: 'istanbul',
       reporter: ['json'],
